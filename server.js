2e3beda6b02a59e021f89cdc298f4772baafdaf9
Jennifer Rosa <rosajen27@gmail.com>
2020-08-08 20:53:20 -0400

    npm i express, update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const app = express();
 
 const PORT = process.env.PORT || 3000
 
-const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,9 +14,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-// routes
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
