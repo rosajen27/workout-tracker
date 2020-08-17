9ef67d55c5fa43453aedc0a1231a201f50943366
Jennifer Rosa <rosajen27@gmail.com>
2020-08-17 15:14:20 -0400

    Update workout.js

--- models/workout.js
+++ models/workout.js
@@ -11,7 +11,7 @@ const WorkoutSchema = new Schema({
     {
       type: { type: String },
       name: { type: String },
-      duration: { type: Number },
+      duration: { type: String },
       weight: { type: Number },
       reps: { type: Number },
       sets: { type: Number },
