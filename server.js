298bd07b0352bfb784a88fbda72f3d3f05fe2bde
Jennifer Rosa <rosajen27@gmail.com>
2020-08-16 20:08:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const db = require("./models");
 
 const port = process.env.PORT || 3000;
 
@@ -9,16 +10,17 @@ const app = express();
 
 app.use(logger("dev"));
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// static pages
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout');
-
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout');
 
 app.listen(port, () => {
   console.log(`App running on port ${port}!`);
