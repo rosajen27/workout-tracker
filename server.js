b5334bb3e37a7aae920219d7be621417e3bcde24
Jennifer Rosa <rosajen27@gmail.com>
2020-08-17 11:32:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 htmlRoutes(app);
 apiRoutes(app);
-mongoose.connect(process.env.MONGODB_URI || "mongodb://<dbuser>:<dbpassword>@ds143131.mlab.com:43131/heroku_4vscr7lc", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds143131.mlab.com:43131/heroku_4vscr7lc", {
   useNewUrlParser: true
 });
 
